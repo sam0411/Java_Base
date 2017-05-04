1037476fb9f48f5958a835958dd63d0b923499ba
sam0411 <sam0411@yeah.net>
2017-05-04 22:43:10 +0800

    update synchronized

--- src/test/java/com/fil/ap/base/thread/TestSynchronized.java
+++ src/test/java/com/fil/ap/base/thread/TestSynchronized.java
@@ -2,31 +2,48 @@ package com.fil.ap.base.thread;
 
 public class TestSynchronized {
 
-	//synchronized, is to lock class
+	//synchronized, for normal method->lock 'this'
+	//synchronized, for static method->lock 'class'
+	
 	public synchronized void m1() {
 		
-		System.out.println("run m1......");
+		System.out.println("m1 lock this......");
 		
 		System.out.println("m1 sleep start......");
 		
-		try {
-			Thread.sleep(50);
-		} catch (InterruptedException e) {
+		synchronized (TestSynchronized.class){
 			
-			e.printStackTrace();
+			System.out.println("m1 lock class......");
+			
+			try {
+				Thread.sleep(50);
+			} catch (InterruptedException e) {
+				
+				e.printStackTrace();
+			}
 		}
-		
+
 		System.out.println("m1 sleep end......");
 	}
 	
 	public synchronized void m2() {
 		
-		System.out.println("run m2......");
+		System.out.println("run m2 with synchronized......");
 	}
 	
 	public void m3() {
 		
-		System.out.println("run m3......");
+		System.out.println("run m3 no synchronized......");
+	}
+	
+	public static void m4static() {
+		
+		System.out.println("run m4 static no synchronized......");
+	}
+	
+	public static void m5static() {
+		
+		System.out.println("run m5 static with synchronized......");
 	}
 	
 	public static void main(String args[]) {
@@ -74,6 +91,12 @@ public class TestSynchronized {
 			
 			System.out.println("enter thread2...");
 			input.m3();
+			TestSynchronized.m4static();
+			
+			synchronized (TestSynchronized.class){
+				TestSynchronized.m5static();
+			}
+			
 			input.m2();
 		}
 	}
